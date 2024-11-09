205505f7d774623c73e9101aebef21a37758a873
kvinaya23 <kulkarnivinaya@outlook.com>
2024-11-08 22:24:44 -0800

    added new code

---
+++ src/variousConcepts/LaunchChrome.java
@@ -0,0 +1,16 @@
+package variousConcepts;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) {
+		System.out.println("Hallo Selenium");
+		Scanner scn = new Scanner(System.in);
+		WebDriver driver =new ChromeDriver();
+	}
+
+}
